cfeb981a0ebbb72aee88d28b0172278b9bc27904
earl spillar <espillar@me.com>
2025-09-28 13:12:25 -0700

    just clarifying syntax

--- show_geo_search.py
+++ show_geo_search.py
@@ -10,11 +10,9 @@ from plotting_vectors import plot_pointing_vectors
 
 def show_geo_search():
     """
-    This demo initializes a simulation structure with the sun and the moon and the earth and time, 
-    then adds a constellation of 12 GEOs with 0.4 radian fields of view. 
-    After propogating to the starting position it does a plot of the satellites with the pointing vectors.
-    It then updates all of the pointing vectors 5 times using pointing_place_update and redisplays the constellation.
-    Update 10 times and redisplay again.
+    This demo initializes a simulation, adds a GEO constellation, and then
+    generates several plots to visualize the satellite pointing updates and
+    the RA/Dec history of one satellite.
     """
     sim_start_time = datetime(2025, 7, 27, 22, 27, 0, tzinfo=timezone.utc)
     sim_data = create_empty_simulation(sim_start_time)
@@ -26,21 +24,46 @@ def show_geo_search():
     
     update_satellite_pointing(sim_data)
     fig1 = plot_pointing_vectors(sim_data, 'Initial Pointing Vectors', sim_start_time)
-    fig1.show()
+
+    ra_history = []
+    dec_history = []
+
+    def record_ra_dec():
+        p = sim_data['satellites']['pointing'][0]
+        p_norm = p / np.linalg.norm(p)
+        ra = np.arctan2(p_norm[1], p_norm[0])
+        dec = np.arcsin(p_norm[2])
+        ra_history.append(np.rad2deg(ra))
+        dec_history.append(np.rad2deg(dec))
+
+    record_ra_dec()
 
     for _ in range(5):
         pointing_place_update(sim_data)
+        update_satellite_pointing(sim_data)
+        record_ra_dec()
     
-    update_satellite_pointing(sim_data)
     fig2 = plot_pointing_vectors(sim_data, 'After 5 Updates', sim_start_time)
-    fig2.show()
 
     for _ in range(10):
         pointing_place_update(sim_data)
+        update_satellite_pointing(sim_data)
+        record_ra_dec()
 
-    update_satellite_pointing(sim_data)
     fig3 = plot_pointing_vectors(sim_data, 'After 15 Updates', sim_start_time)
-    fig3.show()
 
-if __name__ == '__main__':
-    show_geo_search()
+    for _ in range(85):
+        pointing_place_update(sim_data)
+        update_satellite_pointing(sim_data)
+        record_ra_dec()
+
+    fig4 = go.Figure()
+    fig4.add_trace(go.Scatter(y=ra_history, mode='lines', name='RA (deg)'))
+    fig4.add_trace(go.Scatter(y=dec_history, mode='lines', name='Dec (deg)'))
+    fig4.update_layout(
+        title='RA and Dec History of a GEO Satellite',
+        xaxis_title='Update Number',
+        yaxis_title='Angle (degrees)'
+    )
+
+    return (fig1, fig2, fig3, fig4)
